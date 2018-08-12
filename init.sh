7f536eb13211f5684463878d9b6330b8b9e56726
w00y <42319314+w00y@users.noreply.github.com>
2018-08-12 15:30:51 +0100

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,17 @@
+locale-gen en_US.UTF-8
+passwd root
+echo "curl ipinfo.io/ip" >> /usr/bin/ii && chmod +x /usr/bin/ii
+echo "clear && nc -lvp \$1" >> /usr/bin/lll && chmod +x /usr/bin/lll
+echo -e "clear\necho\necho\ncurl --upload-file \$1 https://transfer.sh/\$1;echo ''\n" > /usr/bin/uu && chmod +x /usr/bin/uu
+echo "" >  /etc/ssh/sshd_config
+wget https://raw.githubusercontent.com/cr00w/t00l/master/sshd_config
+cat sshd_config  >>  /etc/ssh/sshd_config
+rm /root/.ssh/authorized_keys
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCmdT+QanBnkH2YpKbtUsXoEC+UqqQvtAzJypNpbkhGqEH57nHw2VWxhKoRJkDTy2CepxDevfAcbJfqVp2I3e7RXl7fYtm2NTVcfrDRVes6b3M4T374plKEmbOvXTZCwjid0CpBm9FBf2L7E0ngP1j82NWm1s3HDvnwWExUzVQTa60OF6A45cDtn81egeI2v5Xq3suM8gSqWG2a77JV0VuAhGLhl+V5Gpyq2NBKFNWD3f47OuZFQPPfU0Rt68GKQrRH7CTAZJiB9BRJravUpaHE5m/EXGq025NcVgXiNzfAdbE1tq7uX0vWkJcSOx/HJtiWgjyvPE4pNiGEOeoOviD/ root@kali' >>  ~/.ssh/authorized_keys
+echo "export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> ~/.bashrc
+chmod 600 ~/.ssh/authorized_keys
+service ssh restart
+apt-get update
+apt install byobu nano xauth software-properties-common locate -y
